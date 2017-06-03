59f630664bf170ad464a6589fb5028d4d2eaab99
omrobbie <putu.roby@yahoo.com>
2017-06-04 00:14:56 +0800

    tambahkan kontrol untuk UI game over

---
+++ Assets/Scripts/GameOverControl.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverControl : MonoBehaviour
+{
+	public Text txScore;
+	public Text txGameOver;
+	public Text txLastScore;
+	public Text labelScore;
+	public Button btnRestart;
+	bool enable = false;
+	public Image btnControl;
+	public Image btnFire;
+	public Text txtFire;
+	public Image panel;
+
+	// Use this for initialization
+	void Start()
+	{
+		SetVisible(false);
+	}
+
+	void SetVisible(bool visible)//visible
+	{
+		panel.enabled = visible;
+		txGameOver.enabled = visible;
+		txLastScore.enabled = visible;
+		labelScore.enabled = visible;
+		btnRestart.gameObject.SetActive(visible);
+		txScore.enabled = !visible;
+		btnControl.enabled = !visible;
+		btnFire.enabled = !visible;
+		txtFire.enabled = !visible;
+		enable = false;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Data.isGameOver && !enable)
+		{
+			txLastScore.text = Data.Score.ToString();
+			SetVisible(true);
+			enable = true;
+		}
+	}
+
+	public void Restart()
+	{
+		Data.isGameOver = false;
+		Data.Score = 0;
+		Application.LoadLevel(0);
+	}
+}
