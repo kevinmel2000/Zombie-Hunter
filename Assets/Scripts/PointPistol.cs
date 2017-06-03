1f89ed7fbc957df90d54eef3f2f9b460bee55673
omrobbie <putu.roby@yahoo.com>
2017-06-03 21:56:56 +0800

    masukkan peluru ke dalam pistol, dan picu saat menekan tombol fire

---
+++ Assets/Scripts/PointPistol.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PointPistol : MonoBehaviour {
+
+	public Rigidbody bullet;
+	[Range(0,100)]
+	public float speed = 30;
+
+	// Update is called once per frame
+	void Update () {
+		if (CrossPlatformInputManager.GetButtonDown ("Jump")) {
+			Rigidbody newBullet = GameObject.Instantiate (bullet, transform.position, transform.rotation);
+			newBullet.velocity = transform.TransformDirection (Vector3.right * speed);
+			Physics.IgnoreCollision (newBullet.GetComponent<SphereCollider> (), transform.parent.GetComponent<BoxCollider> (), true);
+		}
+	}
+}
