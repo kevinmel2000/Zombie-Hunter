a671e12059e1095fdffdc424597375e660a124ad
omrobbie <putu.roby@yahoo.com>
2017-06-03 23:52:34 +0800

    tambahkan UI untuk scoring

---
+++ Assets/Scripts/ShowScore.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowScore : MonoBehaviour {
+
+	public Text txScore;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		txScore.text = Data.Score.ToString();
+	}
+}
