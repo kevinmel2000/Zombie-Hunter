11c3ecfbf0ff6d1a0114cb7f9bd7a8db399093eb
omrobbie <putu.roby@yahoo.com>
2017-06-04 00:46:04 +0800

    tombol play sudah terkoneksi dengan scene gameplay

--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyUp(KeyCode.Escape)) Application.Quit();
+	}
+
+	public void PlayGame() {
+		SceneManager.LoadScene ("GamePlay", LoadSceneMode.Single);
 	}
 }
