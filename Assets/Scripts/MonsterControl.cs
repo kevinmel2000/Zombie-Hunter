f4888aebb908ca3178e8d790e7d204ec457f35d9
omrobbie <putu.roby@yahoo.com>
2017-06-03 23:40:11 +0800

    atur kapan zombie akan keluar

---
+++ Assets/Scripts/MonsterControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class MonsterControl: MonoBehaviour {
+
+	[SerializeField]
+	private GameObject monster;
+	[SerializeField]
+	private GameObject player;
+
+	GameObject[] monsters;
+	float timer = 0;
+
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+
+		if(!Data.isGameOver)
+		if (timer > 5f && GameObject.FindGameObjectsWithTag("Monster").Length < 5)
+		{
+			Vector3 posRecomended;
+			do
+			{
+				posRecomended = new Vector3(Random.Range(100, 180), 0, Random.Range(100, 180));
+			} while ( Vector3.Distance(posRecomended , player.transform.position) < 50f);
+			Instantiate(monster, posRecomended, Quaternion.identity);
+			timer = 0;
+		}
+	}
+}
---
+++ Assets/Scripts/MonsterMove.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class MonsterMove : MonoBehaviour {
+
+	GameObject player;
+	bool isDestroy = false;
+	Animator animator;
+
+	// Use this for initialization
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		animator = gameObject.GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (!Data.isGameOver)
+		{
+			transform.LookAt(player.transform);
+			transform.Translate(Vector3.forward * Time.deltaTime * 1f);
+		}
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag.Equals("Player"))
+		{
+			Data.isGameOver = true;
+			Destroy(gameObject);
+		}
+
+		if (collision.gameObject.tag.Equals("Peluru"))
+		{
+			isDestroy = true;
+			Data.Score++;
+			animator.SetBool("isDead", true);
+			Destroy (gameObject);
+		}
+	}
+}
